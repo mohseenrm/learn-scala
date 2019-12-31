0ec780c4d0c47c485dd1c901a3125a582b8bf58d
mohseenrm <mohseenmukaddam6@gmail.com>
2019-12-30 22:52:13 -0800

    [update] tail end recursion

---
+++ src/playground/TailEndRecursion.scala
@@ -0,0 +1,21 @@
+package playground
+
+object TailEndRecursion extends App {
+	def factorial(value: Int): Int = {
+		if (value <= 1) 1
+		else value * factorial(value-1)
+	}
+
+	println(factorial(5))
+
+	def tailEndFactorial(value: BigInt): BigInt = {
+		def helper(x: BigInt, accumulator: BigInt): BigInt = {
+			if (x <= 1) accumulator
+			else helper(x-1, accumulator * x)
+		}
+
+		helper(value, 1)
+	}
+
+	println(tailEndFactorial(25000))
+}
