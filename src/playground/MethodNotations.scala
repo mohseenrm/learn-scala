1b31b8ccbfd5d3cddcfea1a1d67dee9091a79cf0
mohseenrm <mohseenmukaddam6@gmail.com>
2020-01-02 10:50:25 -0800

    [update] playing with infix/postfix and apply

--- src/playground/MethodNotations.scala
+++ src/playground/MethodNotations.scala
@@ -1,14 +1,27 @@
 package playground
 
-class Person (val name: String, favoriteMovie: String) {
-  def +(person: Person): String = {
-    s"${this.name} is meeting with ${person.name}"
-  }
+class Person (val name: String, favoriteMovie: String, val age: Int = 0) {
+  def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
+  def unary_+ : Person = new Person(name, favoriteMovie, age+1)
+
+  def learns(language: String): String = s"$name learns $language"
+  def learnsScala: String = this learns "Scala"
+
+  def apply(times: Int): String = s"$name watched $favoriteMovie $times times"
 }
 
 object MethodNotations extends App {
-  val momo = new Person("Mohseen", "Godfather")
-  val panda = new Person("Utkarsh", "Dil mil")
+  val mohseen = new Person("Mohseen", "Godfather")
+  // val panda = new Person("Utkarsh", "Dil mil")
+  val momo = mohseen + "momo"
 
-  println(momo + panda)
+  // Get new person with added nickname
+  println(momo.name)
+  // Get new person with incremented age
+  println((+momo).age)
+
+  println(momo learns "Scala")
+  println(momo learnsScala)
+  // Overload the apply operator
+  println(momo(3))
 }
